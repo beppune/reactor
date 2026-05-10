96b8427c4a8ae20afbc176189e6e9665954ea311
beppune <manzo.giuseppe@gmail.com>
2026-05-10 22:09:27 +0200

    FileWriterHandler

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,23 @@ use reactor::Reactor;
 
 fn main() {
 
+
     let mut rct = Reactor::new();
 
-    let _ = rct.read_file("Cargo.toml");
+    let _ = rct.read_file("Cargo.toml", |data, n| {
+
+        let s = String::from_utf8(data).unwrap();
+        println!("{n} bytes red\n{s}");
+
+    });
+
+    let buffer:String = String::from("HEllo\n");
+
+    let _ = rct.write_file("text.txt", Vec::from(buffer), |_data, n|{
+
+        println!("wrote bytes: {n}");
+
+    });
 
     rct.run();
 }
--- src/reactor.rs
+++ src/reactor.rs
@@ -1,35 +1,75 @@
+use std::borrow::Borrow;
 use std::collections::HashMap;
 use std::vec;
-use std::os::fd::{AsFd, AsRawFd, OwnedFd, RawFd};
+use std::os::fd::{AsFd, AsRawFd, BorrowedFd, OwnedFd, RawFd};
 use std::io::{self};
 
+use nix::errno::Errno;
 use nix::fcntl::{self, OFlag};
-use nix::libc::O_RDONLY;
+use nix::libc::{O_CREAT, O_RDONLY, O_WRONLY, POLLIN, POLLOUT};
 use nix::{poll::{PollFd, PollFlags, PollTimeout}, sys::stat::Mode};
 
 enum Interest {
     Read,
+    Write,
 }
 
 pub enum Action {
-    Stop, 
+    Stop,
+    Continue, 
 }
 
 pub trait Handler {
-    fn handle(&mut self, fd: OwnedFd) -> Action;
+    fn handle(&mut self, fd: BorrowedFd) -> Action;
+}
+
+struct FileWriterHandler {
+    buffer: Vec<u8>,
+    complete: Option<Box<dyn FnOnce(Vec<u8>, usize)>>,
+}
+
+impl Handler for FileWriterHandler {
+    fn handle(&mut self, fd: BorrowedFd) -> Action {
+        let action:Action;
+
+        match nix::unistd::write(fd, &mut self.buffer) {
+            Ok(n) => {
+                let data = std::mem::take(&mut self.buffer);
+                let cb = std::mem::take(&mut self.complete).unwrap();
+
+                (cb)(data, n);
+                action = Action::Stop;
+            },
+            Err(e) if e == Errno::EAGAIN => action = Action::Continue,
+            Err(_) => action = Action::Stop,
+        }
+
+        action
+    }
 }
 
 struct FileReadHandler {
     buffer: Vec<u8>,
+    complete: Option<Box<dyn FnOnce(Vec<u8>, usize)>>,
 }
 
 impl Handler for FileReadHandler {
-    fn handle(&mut self, fd: OwnedFd) -> Action {
-        self.buffer.clear();
-        let res = nix::unistd::read(fd, &mut self.buffer);
-        let _ = dbg!(res);
-        dbg!(&self.buffer);
-        Action::Stop        
+    fn handle(&mut self, fd: BorrowedFd) -> Action {
+        let action;
+        match nix::unistd::read(fd, &mut self.buffer) {
+            Ok(n) => {
+                let data = std::mem::take(&mut self.buffer);
+                let cb = std::mem::take(&mut self.complete).unwrap();
+
+                (cb)(data, n);
+
+                action = Action::Stop
+            },
+            Err(e) if e == Errno::EAGAIN => action = Action::Continue,
+            Err(_) => action = Action::Stop,
+        }
+
+        action
     }
 }
 
@@ -46,35 +86,68 @@ impl Reactor {
     }
 
     pub fn run(&mut self) {
-        // poll
-        let mut topolls:Vec<RawFd> = vec![];
-        {
-            let mut p:Vec<PollFd> = self.fds.values().map(|(ofd, _, _)| PollFd::new( ofd.as_fd(), PollFlags::POLLIN) ).collect();
-            let n = nix::poll::poll(&mut p, PollTimeout::NONE).unwrap();
-            if n > 0 {
-                topolls = p.iter().map(|k|k.as_fd().as_raw_fd()).collect();
+        loop {
+            if self.fds.is_empty() {
+                break;
             }
-        }
-        // demux
-        for fd in topolls {
-            let (ofd, mut hnd, _int) = self.fds.remove(&fd).unwrap();
-            let action = hnd.handle(ofd);
 
-            match action {
-                Action::Stop => println!("Stop"),
+            // poll
+            let mut topolls:Vec<RawFd> = vec![];
+            {
+                let mut p:Vec<PollFd> = self.fds.values().map(|(ofd, _, _)| PollFd::new( ofd.as_fd(), PollFlags::from_bits(POLLIN | POLLOUT).unwrap()) ).collect();
+                let n = nix::poll::poll(&mut p, PollTimeout::NONE).unwrap();
+                if n > 0 {
+                    topolls = p.iter()
+                        .filter(|p| p.revents().is_some_and(|f| !f.is_empty()))
+                        .map(|k|k.as_fd().as_raw_fd())
+                        .collect();
+                }
             }
+            // demux
+            for fd in topolls {
+                let (ofd, hnd, _int) = self.fds.get_mut(&fd).unwrap();
+                let action = hnd.handle(ofd.as_fd());
+
+                match action {
+                    Action::Stop => { 
+                        self.fds.remove(&fd).unwrap();
+                    },
+                    Action::Continue => {
+
+                    },
+                }
+            }
+            // demux
         }
+        // loop
     }
 
-    pub(crate) fn read_file(&mut self, path:&str) -> io::Result<()> {
+    pub fn read_file(&mut self, path:&str, cb:impl FnOnce(Vec<u8>, usize) +'static ) -> io::Result<()> {
         let ofd = fcntl::open(path, OFlag::from_bits(O_RDONLY).unwrap(), Mode::empty())?;
         let fd = ofd.as_raw_fd();
 
         let h = Box::new(FileReadHandler {
-            buffer: vec![]
+            buffer: vec![0; 512],
+            complete: Some(Box::new(cb)),
         });
 
         self.fds.insert(fd, (ofd, h, Interest::Read) );
+
+        Ok(())
+    }
+
+    pub fn write_file(&mut self, path: &str, buffer:Vec<u8>, cb: impl FnOnce(Vec<u8>, usize) + 'static ) -> io::Result<()> {
+
+        let ofd = fcntl::open(path, OFlag::from_bits(O_WRONLY|O_CREAT).unwrap(), Mode::empty())?;
+
+        let fd = ofd.as_raw_fd();
+
+        let h = Box::new(FileWriterHandler {
+            buffer,
+            complete: Some(Box::new(cb)),
+        });
+
+        self.fds.insert(fd, (ofd, h, Interest::Write));
 
         Ok(())
     }
